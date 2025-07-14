3945a8804e80b004cd8b1cde4db2923dfbc2269c
Thomas Dürr <thomduerr@gmail.com>
2025-07-14 22:21:09 +0200

    tool

--- llm_tools_weather_forecast.py
+++ llm_tools_weather_forecast.py
@@ -1,12 +1,19 @@
 import llm
+import os
+import requests
+import json
 
-
-def weather_forecast(input: str) -> str:
+def weather_forecast(city_name: str) -> str:
     """
-    Description of tool goes here.
+    This function predicts the weather for the current and future days based on the name of a city.
     """
-    return f"hello {input}"
-
+    try:
+        api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+        url = "http://api.openweathermap.org/data/2.5/weather?appid=" + api_key + "&q=" + city_name
+        data = requests.get(url).json()
+        return f'{data["name"]}, {round(int(data["main"]["temp"])-273.15, 1)}°C, {data["weather"][0]["description"]}'
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 @llm.hookimpl
 def register_tools(register):
