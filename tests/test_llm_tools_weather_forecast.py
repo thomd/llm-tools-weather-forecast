d630a517fb8f68a924a53c324546ce51ca3663f0
Thomas Dürr <thomduerr@gmail.com>
2025-07-21 17:05:44 +0200

    pytest

--- tests/test_llm_tools_weather_forecast.py
+++ tests/test_llm_tools_weather_forecast.py
@@ -1,21 +1,35 @@
 import llm
 import json
+import pytest
+from unittest.mock import patch, Mock
 from llm_tools_weather_forecast import weather_forecast
 
-def test_tool():
+@patch('llm_tools_weather_forecast.requests.get')
+def test_tool(mock_get):
+
+    def mock_requests(url):
+        mock_response = Mock()
+        if 'geo/1.0/direct' in url:
+            mock_response.json.return_value = [{"lat": 0.0000, "lon": 0.0000}]
+        elif 'data/2.5/forecast' in url:
+            mock_response.json.return_value = {}
+        return mock_response
+    mock_get.side_effect = mock_requests
+
     model = llm.get_model("echo")
     chain_response = model.chain(
         json.dumps(
             {
                 "tool_calls": [
-                    {"name": "weather_forecast", "arguments": {"input": "Hamburg"}}
+                    {"name": "weather_forecast", "arguments": {"city_name": "Hamburg"}}
                 ]
             }
         ),
         tools=[weather_forecast],
     )
     responses = list(chain_response.responses())
+
     tool_results = json.loads(responses[-1].text())["tool_results"]
     assert tool_results == [
-        {"name": "weather_forecast", "output": "hello pelican", "tool_call_id": None}
+        {"name": "weather_forecast", "output": "{}", "tool_call_id": None}
     ]
