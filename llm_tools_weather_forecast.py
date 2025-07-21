225c233eab5da9f421660b197c828b851eafb848
Thomas Dürr <thomduerr@gmail.com>
2025-07-21 17:13:04 +0200

    exception

--- llm_tools_weather_forecast.py
+++ llm_tools_weather_forecast.py
@@ -9,6 +9,8 @@ def weather_forecast(city_name: str) -> str:
     """
     try:
         api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+        if api_key == None:
+            raise Exception('API key for OpenWeatherMap missing')
         geocode_url =  f'https://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={api_key}'
         geocode_data = requests.get(geocode_url).json()
         lat, lon = geocode_data[0]['lat'], geocode_data[0]['lon']
